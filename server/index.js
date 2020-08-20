e64cfe8cddb6e372f06b092f09abfb4d5bf2aacc
Shane Walsh <shanewalsh@hey.com>
2020-08-20 14:41:47 +0100

    adds comments to stop eslint complainting

--- server/index.js
+++ server/index.js
@@ -8,11 +8,14 @@ const app = express();
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 
+// eslint-disable-next-line no-console
 db.on('error', error => console.log((error)));
+// eslint-disable-next-line no-console
 db.once('open', () => console.log('db connected'));
 
 app.use(express.json());
 
 app.use('/dogs', dogsRouter);
 
+// eslint-disable-next-line no-console
 app.listen(3000, () => console.log('server started'));

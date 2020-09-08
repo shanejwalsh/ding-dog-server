d5637c33474f775f8670e2eb423970c29ac23d5d
Shane Walsh <shanewalsh@hey.com>
2020-09-08 23:36:15 +0100

    replaces credentials in permissions string

--- network/server/index.js
+++ network/server/index.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const serverless = require('serverless-http');
-
 const mongoose = require('mongoose');
 const dogsRouter = require('./routes/dogs');
 
 const app = express();
 
-const atlasDB = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@myfirstcluster.kv7fp.mongodb.net/DogsTrustAPI?retryWrites=true&w=majority`;
+const atlasDB = 'mongodb+srv://shanejwalsh:nT0zUSOCmSlUfzG8@myfirstcluster.kv7fp.mongodb.net/DogsTrustAPI?retryWrites=true&w=majority';
 
 mongoose.connect(atlasDB, {
     useNewUrlParser: true,

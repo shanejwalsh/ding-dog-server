57a91859ae8e626b930c802878604a37286d1d3c
Shane Walsh <shanewalsh@hey.com>
2020-09-08 22:53:41 +0100

    adds correct db name to db string

--- network/server/index.js
+++ network/server/index.js
@@ -7,11 +7,9 @@ const dogsRouter = require('./routes/dogs');
 
 const app = express();
 
-const atlasDB = 'mongodb+srv://shanejwalsh:nT0zUSOCmSlUfzG8@myfirstcluster.kv7fp.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const atlasDB = 'mongodb+srv://shanejwalsh:nT0zUSOCmSlUfzG8@myfirstcluster.kv7fp.mongodb.net/DogsTrustAPI?retryWrites=true&w=majority';
 
 mongoose.connect(atlasDB, {
-    user: process.env.ATLAS_USER,
-    pass: process.env.ATLAS_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });

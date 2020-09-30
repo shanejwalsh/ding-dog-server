a9dadf6d9a08883e08128aa5afc919e789eae06f
Shane Walsh <shanewalsh@hey.com>
2020-09-30 22:51:53 +0100

    adds model for dogs that have previously been adopted

---
+++ models/adoptedDog.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const dogSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    breed: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    imgSrc: {
+        type: String,
+    },
+    adoptedAt: {
+        type: Date,
+    },
+});
+
+module.exports = mongoose.model('Dog', dogSchema);
